fd37ad6dfbee5590cdbefcadfe3b7172058c0fae
mhbratek <mhbratek@gmail.com>
2020-03-15 22:52:13 +0100

    New task.

---
+++ src/main/java/com/bratek/CountNegativeNumbersInSortedMatrix.java
@@ -0,0 +1,7 @@
+package com.bratek;
+
+public class CountNegativeNumbersInSortedMatrix {
+    public static int countNegatives(int[][] grid) {
+        return 8;
+    }
+}
---
+++ src/test/java/com/bratek/CountNegativeNumbersInSortedMatrixTest.java
@@ -0,0 +1,17 @@
+package com.bratek;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountNegativeNumbersInSortedMatrixTest {
+
+    @Test
+    public void shouldReturnNumberOfNegativeNumbers() {
+        Assertions.assertEquals(
+                8,
+                CountNegativeNumbersInSortedMatrix.countNegatives(new int[][] {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}})
+        );
+    }
+}
