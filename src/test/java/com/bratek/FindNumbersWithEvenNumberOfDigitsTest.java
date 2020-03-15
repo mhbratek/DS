02db1bc41a568216eef70e4336897f3fabfe4c9d
mhbratek <mhbratek@gmail.com>
2020-03-15 19:11:46 +0100

    Fix imports.

--- src/test/java/com/bratek/FindNumbersWithEvenNumberOfDigitsTest.java
+++ src/test/java/com/bratek/FindNumbersWithEvenNumberOfDigitsTest.java
@@ -3,8 +3,6 @@ package com.bratek;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FindNumbersWithEvenNumberOfDigitsTest {
 
     @Test
