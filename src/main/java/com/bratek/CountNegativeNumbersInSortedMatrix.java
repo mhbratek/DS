b88d779c8e8e00a8a96e60eb38a46f2ba8a42f4b
mhbratek <mhbratek@gmail.com>
2020-03-15 23:06:54 +0100

    Solution.

--- src/main/java/com/bratek/CountNegativeNumbersInSortedMatrix.java
+++ src/main/java/com/bratek/CountNegativeNumbersInSortedMatrix.java
@@ -2,6 +2,14 @@ package com.bratek;
 
 public class CountNegativeNumbersInSortedMatrix {
     public static int countNegatives(int[][] grid) {
-        return 8;
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            int j = grid[i].length - 1;
+            while (j >= 0 && grid[i][j] < 0) {
+                result++;
+                j--;
+            }
+        }
+        return result;
     }
 }
--- src/test/java/com/bratek/CountNegativeNumbersInSortedMatrixTest.java
+++ src/test/java/com/bratek/CountNegativeNumbersInSortedMatrixTest.java
@@ -3,7 +3,6 @@ package com.bratek;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class CountNegativeNumbersInSortedMatrixTest {
 
