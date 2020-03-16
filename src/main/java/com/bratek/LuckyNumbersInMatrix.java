67d717244462dd0ca3f25e6f18c060111661a541
mhbratek <mhbratek@gmail.com>
2020-03-16 20:21:43 +0100

    Prepare for solution.

---
+++ src/main/java/com/bratek/LuckyNumbersInMatrix.java
@@ -0,0 +1,10 @@
+package com.bratek;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LuckyNumbersInMatrix {
+    public static List<Integer> luckyNumbers (int[][] matrix) {
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/java/com/bratek/LuckyNumbersInMatrixTest.java
@@ -0,0 +1,20 @@
+package com.bratek;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LuckyNumbersInMatrixTest {
+
+    @Test
+    public void shouldReturnListOfLuckyNumbers() {
+        Assertions.assertEquals(
+                Collections.singletonList(15),
+                LuckyNumbersInMatrix.luckyNumbers(new int[][]{{3, 7, 8}, {9, 11, 13}, {15, 16, 17}})
+        );
+    }
+}
