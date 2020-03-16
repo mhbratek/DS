550ed13074de850f60ba43d3c17478872058d4de
mhbratek <mhbratek@gmail.com>
2020-03-16 19:17:59 +0100

    Solution.

--- src/main/java/com/bratek/ReplaceElementsWithGreatestElementOnRightSide.java
+++ src/main/java/com/bratek/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -2,6 +2,20 @@ package com.bratek;
 
 public class ReplaceElementsWithGreatestElementOnRightSide {
     public static int[] replaceElements(int[] arr) {
-        return new int[] {1};
+        for (int i = 1; i < arr.length; i++) {
+            arr[i - 1] = findMax(i, arr);
+        }
+        arr[arr.length - 1] = -1;
+        return arr;
+    }
+
+    private static int findMax(int begin, int[] arr) {
+        int max = 0;
+        for (int i = begin; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
     }
 }
